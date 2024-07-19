e9cb652da598fde44a685bdc1804cf8565b3894d
ABK <98167690+aditya8975@users.noreply.github.com>
2024-07-19 09:39:15 +0530

    Create auth.js

---
+++ config/auth.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/User');
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.redirect('/login');
+  } catch (err) {
+    res.redirect('/register');
+  }
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout(() => {
+    res.redirect('/login');
+  });
+});
+
+module.exports = router;
